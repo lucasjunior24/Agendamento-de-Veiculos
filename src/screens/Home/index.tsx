69e1cc8f9117cbd5ab63c47b67483ddb349eb471
lucasjunior24 <lukateku@gmail.com>
2021-12-13 23:26:26 -0300

    Criado Header da home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import { StatusBar } from 'react-native';
+import { RFValue } from "react-native-responsive-fontsize";
 
-import { Container, Title } from './styles';
+import Logo from '../../assets/logo.svg';
+
+import { Container, Header, TotalCars, HeaderContent } from './styles';
 
 export function Home() {
     return (
         <Container>
-            <Title>HOME MAN</Title>
+            <StatusBar 
+                barStyle="light-content"  />
+            <Header>
+                <HeaderContent>
+                    <Logo width={RFValue(108)} height={RFValue(12)} />
+                    <TotalCars>
+                        Total de 12 carros
+                    </TotalCars>
+                </HeaderContent>
+            </Header>
         </Container>
     );
 }
