1c87fd91c85e17587db8f2a19e1c02e2eee86192
lucasjunior24 <lukateku@gmail.com>
2021-12-15 20:20:03 -0300

    Criado interface de detalhes dos carros

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,10 +5,10 @@ import { RFValue } from "react-native-responsive-fontsize";
 import Logo from '../../assets/logo.svg';
 import { Car } from "../../components/Car";
 
-import { Container, Header, TotalCars, HeaderContent } from './styles';
+import { Container, Header, TotalCars, HeaderContent, CarList } from './styles';
 
 export function Home() {
-    const carDataOne = {
+    const carData = {
         brand: 'Audi',
         name: "RS 5 Coupé",
         rent: {
@@ -16,16 +16,6 @@ export function Home() {
             price: 120,
         },
         thumbnael: 'https://img1.gratispng.com/20171220/kiq/audi-png-car-image-5a3b1f1eb47de9.9104985015138240307393.jpg'
-    }
-
-    const carDataTwe = {
-        brand: 'Porshe',
-        name: "Ponamera",
-        rent: {
-            period: 'AO DIA',
-            price: 120,
-        },
-        thumbnael: 'https://img1.gratispng.com/20180203/fuw/kisspng-porsche-930-2015-porsche-911-turbo-s-car-turbochar-gold-porsche-911-turbo-aerokit-car-5a75571960ffa9.5643006815176394493973.jpg'
     }
 
     return (
@@ -40,8 +30,11 @@ export function Home() {
                     </TotalCars>
                 </HeaderContent>
             </Header>
-            <Car data={carDataOne} />
-            <Car data={carDataTwe} />
+            <CarList 
+                data={[1,2,3,4,5,6,7]} 
+                keyExtractor={item => String(item)}
+                renderItem={({ item }) => <Car data={carData} /> } 
+            />
         </Container>
     );
 }
