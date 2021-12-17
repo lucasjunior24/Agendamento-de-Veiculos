481c4b8898730593659b69411c4ad0474edd8691
lucasjunior24 <lukateku@gmail.com>
2021-12-16 23:09:12 -0300

    Criado component de Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
-import { Container, Header } from './styles';
+import { Container, Header, CarImages } from './styles';
 
 export function CarDetails() {
     return (
@@ -9,6 +10,12 @@ export function CarDetails() {
             <Header>
                 <BackButton onPress={() => {}} />
             </Header>
+
+            <CarImages>
+                <ImageSlider 
+                    imagesUrl={['https://img1.gratispng.com/20171220/kiq/audi-png-car-image-5a3b1f1eb47de9.9104985015138240307393.jpg']} 
+                />
+            </CarImages>
         </Container>
     );
 }
