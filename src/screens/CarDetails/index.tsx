597c6721b39bd201b56ee378c1e95f2f8b737dbc
lucasjunior24 <lukateku@gmail.com>
2021-12-18 12:04:25 -0300

    Continuando interface de detalhes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 
-import { Container, Header, CarImages } from './styles';
+import { 
+    Container, 
+    Header, 
+    CarImages,
+    Content,
+    Details,
+    Description,
+    Brand,
+    Name,
+    Rent,
+    Period,
+    Price,
+    About
+} from './styles';
 
 export function CarDetails() {
     return (
@@ -16,6 +29,24 @@ export function CarDetails() {
                     imagesUrl={['https://img1.gratispng.com/20171220/kiq/audi-png-car-image-5a3b1f1eb47de9.9104985015138240307393.jpg']} 
                 />
             </CarImages>
+
+            <Content>
+                <Details>
+                    <Description>
+                        <Brand>Lamborghini</Brand>
+                        <Name>Huracan</Name>
+                    </Description>
+
+                    <Rent>
+                        <Period>Ao Dia</Period>
+                        <Price>R$ 549,90</Price>
+                    </Rent>
+                </Details>
+
+                <About>
+                    Lamborghini é uma fabricante italiana de automóveis desportivos de luxo e de alto desempenho criada originalmente para competir com a Ferrari com sede no município de Sant'Agata Bolognese.
+                </About>
+            </Content>
         </Container>
     );
 }
