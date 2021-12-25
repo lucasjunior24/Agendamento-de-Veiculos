b5b91d2c466b0c6b717239f82ae8d1198a903be8
lucasjunior24 <lukateku@gmail.com>
2021-12-24 21:34:43 -0300

    Criado tela de confirmação do agendamento

--- App.tsx
+++ App.tsx
@@ -17,7 +17,8 @@ import {
 import theme from './src/styles/theme';
 // import { CarDetails } from './src/screens/CarDetails';
 // import { Scheduling } from './src/screens/Scheduling';
-import { SchedulingDetails } from './src/screens/SchedulingDetails';
+// import { SchedulingDetails } from './src/screens/SchedulingDetails';
+import { SchedulingComplete } from './src/screens/SchedulingComplete';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -33,7 +34,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <SchedulingDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   );
 }
